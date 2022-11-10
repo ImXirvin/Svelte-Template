fc33b9ef33bc7eebe9792f4f7bee470276c9db48
Xirvin <xander228irvin@gmail.com>
2022-11-10 11:57:37 +1100

    Remove esc mount and Backdrop in default

--- ui/src/providers/VisibilityProvider.svelte
+++ ui/src/providers/VisibilityProvider.svelte
@@ -17,26 +17,26 @@
   });
 
 
-  onMount(() => {
-    const keyHandler = (e: KeyboardEvent) => {
-      if (isVisible && ['Escape'].includes(e.code)) {
-        SendNUI('hideUI');
-        visibility.set(false);
-      }
-    };
+  // onMount(() => {
+  //   const keyHandler = (e: KeyboardEvent) => {
+  //     if (isVisible && ['Escape'].includes(e.code)) {
+  //       SendNUI('hideUI');
+  //       visibility.set(false);
+  //     }
+  //   };
 
-    window.addEventListener('keydown', keyHandler);
+  //   window.addEventListener('keydown', keyHandler);
   
 
-    return () => window.removeEventListener('keydown', keyHandler);
-  });
+  //   return () => window.removeEventListener('keydown', keyHandler);
+  // });
 </script>
 
 {#if isVisible}
 <main>
     <slot />
 </main>
-<BackdropFix />
+<!-- <BackdropFix /> -->
 {/if}
 
 <style>
