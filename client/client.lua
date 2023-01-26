e8af8b1cc9b40d2b367890351daa71c378d5417b
Xirvin <xander228irvin@gmail.com>
2023-01-26 13:50:36 +1100

    hideUI Event and default escape event

--- client/client.lua
+++ client/client.lua
@@ -4,3 +4,12 @@
 --     action = "setVisible",
 --     data = true
 -- })
+
+-- RegisterNUICallback("hideUI", function()
+--     SetNuiFocus(false, false)
+--     SendNUIMessage({
+--         action = "setVisible",
+--         data = false
+--     })
+--     ui = false
+-- end)
