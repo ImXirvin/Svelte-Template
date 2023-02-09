bab6abd874973985137dde7a2a38d51fe5600008
Xirvin <xander228irvin@gmail.com>
2023-02-09 17:41:28 +1100

    small fix

--- ui/src/utils/SendNUI.ts
+++ ui/src/utils/SendNUI.ts
@@ -1,5 +1,15 @@
 import { browserMode, resName } from '@store/stores'
 
+let isBrowserMode: boolean = false;
+browserMode.subscribe((value: boolean) => {
+  isBrowserMode = value;
+});
+
+let debugResName: string = "";
+resName.subscribe((value: string) => {
+  debugResName = value;
+});
+
 /**
 * @param eventName - The endpoint eventname to target
 * @param data - Data you wish to send in the NUI Callback
@@ -7,23 +17,13 @@ import { browserMode, resName } from '@store/stores'
 * @return returnData - A promise for the data sent back by the NuiCallbacks CB argument
 */
 
-let isBrowserMode = false;
-browserMode.subscribe((value) => {
-  isBrowserMode = value;
-});
-
-let debugResName = "";
-resName.subscribe((value) => {
-  debugResName = value;
-});
-
 export async function SendNUI<T = any>(
   eventName: string,
   data: unknown = {},
   debugReturn?: T
 ): Promise<T> {
   if (isBrowserMode == true && debugReturn) {
-    return Promise.resolve(debugReturn as T)
+    return Promise.resolve(debugReturn || {} as T)
   }
   const options = {
     method: "post",
@@ -37,7 +37,6 @@ export async function SendNUI<T = any>(
     ? (window as any).GetParentResourceName()
     : debugResName;
 
-    if (isBrowserMode) return debugReturn || {} as T;
 
     const resp: Response = await fetch(`https://${resourceName}/${eventName}`, options);
     return await resp.json()
