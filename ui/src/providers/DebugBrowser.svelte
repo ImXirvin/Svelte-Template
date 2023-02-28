dc2a08789193077f809e08f3c9728241b77422e5
Xirvin <xander228irvin@gmail.com>
2023-02-28 13:46:14 +1100

    Update debug Options

--- ui/src/providers/DebugBrowser.svelte
+++ ui/src/providers/DebugBrowser.svelte
@@ -13,17 +13,45 @@
 
 	let options = [
 		{
-			component: 'example',
-			action: 'showEvent',
-			data: {
-				example: true
-			},
-			closeAction: 'hideEvent',
+				component: 'Show',
+				actions : [
+					{
+						name: "show",
+						action: "setVisible",
+						data: true,
+					},
+					{
+						name: "hide",
+						action: "setVisible",
+						data: false,
+					},
+				]
 		},
+		// {
+		// 	component: 'Example',
+		// 	actions : [
+		// 		{
+		// 			name: "debugLocation",
+		// 			custom: true,
+		// 			customFunction: () => {
+					// CUSTOM LOGIC
+		// 			}
+		// 		},
+		// 		{
+		// 			name: "toggle Show",
+		// 			custom: true,
+		// 			customFunction: () => {
+					// CUSTOM LOGIC
+		// 			}
+		// 		},
+		// 	]
+		// },
+
 	]
 </script>
 
-<div class="absolute top-0 left-0 z-[1000]">
+
+<div class="absolute top-0 left-1/2 z-[1000]">
 	<button class="bg-red-500 text-white p-2"
 		on:click={() => {
 			show = !show;
@@ -32,34 +60,29 @@
 	Show
 	</button>
 	{#if show}
-	<div class="w-fit h-fit bg-gray-600 p-2">
+	<div class="w-fit h-fit bg-gray-600 p-2 ">
 		{#each options as option}
 		<div class="flex flex-row gap-2 items-center m-1">
 			<p class="text-white">{option.component}</p>
-			<button class="bg-red-500 text-white p-2"
+			{#each option.actions as action}
+			<button class="bg-blue-500 text-white p-2"
 				on:click={() => {
+
+					if (action.custom == true) {
+						action.customFunction();
+						return
+					}
 					debugData([
 						{
-							action: option.action,
-							data: option.data,
+							action: action.action,
+							data: action.data,
 						},
 					])
 				}}
 			>
-				Start
+			{action.name}
 			</button>
-			<button class="bg-red-500 text-white p-2"
-				on:click={() => {
-					debugData([
-						{
-							action: option.closeAction,
-							data: option.data,
-						},
-					])
-				}}
-			>
-				Close
-			</button>
+			{/each}
 		</div>
 		{/each}
 	</div>
