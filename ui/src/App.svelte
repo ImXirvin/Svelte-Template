29aa2446d07d50491c48359d632825c31544d481
Xirvin <xander228irvin@gmail.com>
2023-05-16 13:49:21 +1000

    debug image

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -12,14 +12,19 @@
 </VisibilityProvider>
 
 
-<style>
-
-
-</style>
-
-
 <AlwaysListener />
 {#if $BROWSER_MODE}
 	<DebugBrowser />
-
+	<div class="absolute w-screen h-screen top-0 left-0 dev-image" />
 {/if}
+
+
+<style>
+	.dev-image {
+		background-image: url('https://cdn.discordapp.com/attachments/1068663854143389788/1074878298498281554/HUD_-_Target_Menu.png');
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+
+	}
+</style>
